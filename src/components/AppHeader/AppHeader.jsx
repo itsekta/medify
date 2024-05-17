4dd91bf0fcb5d018074cc5c5c3d05581e889d1a2
Ekta Chaudhary <ektachaudhary129@gmail.com>
2024-05-17 23:21:45 +0530

    WIP: hero section

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,62 +1,88 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import style from "./AppHeader.module.css";
-import Container from "@mui/material/Container";
-import SvgIcon from "@mui/material/SvgIcon";
+import {
+  Box,
+  Container,
+  Button,
+  Stack,
+  Typography,
+  useMediaQuery,
+  IconButton,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import styles from "./AppHeader.module.css";
+import { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
 
-function AppHeader() {
+export default function AppHeader() {
+  const isMobile = useMediaQuery("(max-width:900px)");
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-      <Box
-        sx={{
-          p: 2,
-          background:
-            "linear-gradient(81deg, #E7F0FF 9.01%, rgba(232, 241, 255, 0.47) 89.11%)",
-        }}
-      >
-        <Container maxWidth="lg">
-          <div className={style.appHeaderWrap}>
-            <div className={style.appHeaderLogo}>
-              <div className={style.appHeaderIconWrap}>
-                <SvgIcon sx={{ fontSize: "14px" }}>
-                  <svg
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M0.862605 2.83005C2.90469 2.8023 4.76526 2.02253 6.1804 0.754883C7.59554 2.02253 9.45611 2.8023 11.4982 2.83005C11.5724 3.27214 11.611 3.72632 11.611 4.18951C11.611 7.73924 9.34424 10.759 6.1804 11.8782C3.01656 10.759 0.749847 7.73924 0.749847 4.18951C0.749847 3.72632 0.788449 3.27214 0.862605 2.83005ZM8.66862 5.32103C8.91811 5.07133 8.91811 4.66645 8.66862 4.41676C8.41913 4.16707 8.01459 4.16707 7.7651 4.41676L5.50158 6.68215L4.5957 5.77552C4.34622 5.52583 3.94167 5.52583 3.69219 5.77552C3.4427 6.02522 3.4427 6.4301 3.69219 6.67979L5.04983 8.03855C5.29931 8.28824 5.70386 8.28824 5.95334 8.03855L8.66862 5.32103Z"
-                      fill="white"
-                    />
-                  </svg>
-                </SvgIcon>
-              </div>
-              <div>Medify</div>
-            </div>
-            <div className={style.appHeaderLinks}>
-              <div>Find Doctors</div>
-              <div>Hospital</div>
-              <div>Medicines</div>
-              <div>Surgeries</div>
-              <div>Software for Provider</div>
-              <div>Facilities</div>
-              <Button
-                className={style.appHeaderButton}
-                variant="contained"
-                style={{ textTransform: "capitalize" }}
-              >
+    <header>
+      <Box p={1} bgcolor="primary.main">
+        <Typography fontSize={14} textAlign="center" color="#fff">
+          The health and well-being of our patients and their health care team
+          will always be our priority, so we follow the best practices for
+          cleanliness.
+        </Typography>
+      </Box>
+
+      <Container maxWidth="xl">
+        <Stack
+          direction="row"
+          spacing={2}
+          alignItems="center"
+          justifyContent="space-between"
+          py={2}
+        >
+          <Link to="/">
+            <img src={logo} alt="Logo" height={27} />
+          </Link>
+
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            spacing={4}
+            alignItems={{ xs: "flex-start", md: "center" }}
+            className={[styles.navlinks, menuOpen && styles.active]}
+            pt={{ xs: 12, md: 1 }}
+            pb={{ xs: 4, md: 1 }}
+            px={{ xs: 4, md: 0 }}
+          >
+            <Link>Find Doctors</Link>
+            <Link to="/search">Hospitals</Link>
+            <Link>Medicines</Link>
+            <Link>Surgeries</Link>
+            <Link>Software for Provider</Link>
+            <Link>Facilities</Link>
+            <Link to="/my-bookings">
+              <Button variant="contained" disableElevation>
                 My Bookings
               </Button>
-            </div>
-          </div>
-        </Container>
-      </Box>
-    </>
+            </Link>
+
+            {isMobile && (
+              <IconButton
+                onClick={() => setMenuOpen(false)}
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  right: 32,
+                  color: "#fff",
+                }}
+              >
+                <CloseIcon />
+              </IconButton>
+            )}
+          </Stack>
+
+          {isMobile && (
+            <IconButton onClick={() => setMenuOpen(true)}>
+              <MenuIcon />
+            </IconButton>
+          )}
+        </Stack>
+      </Container>
+    </header>
   );
 }
-
-export default AppHeader;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,37 @@
-import HeadlineComponent from "../components/HeadlineComponent/HeadlineComponent";
+import { Container, Box, Stack } from "@mui/material";
 import AppHeader from "../components/AppHeader/AppHeader";
+import HeroSection from "../components/HeroSection/HeroSection";
+import SearchInput from "../components/SearchCard/SearchInput";
+import SearchCard from "../components/SearchCard/SearchCard";
 
-function HomePage() {
+export default function HomePage() {
   return (
-    <>
-      <HeadlineComponent />
-      <AppHeader />
-    </>
+    <Box>
+      <Box
+        sx={{
+          background:
+            "linear-gradient(#E7F0FF , rgba(232, 241, 255, 0.47) 90%, #fff 10%)",
+        }}
+        mb={4}
+      >
+        <AppHeader />
+        <Container maxWidth="xl">
+          <HeroSection />
+          <Stack
+            p={{ xs: 2.5, md: 8 }}
+            mt={{ xs: -2, md: 0, lg: -6, xl: -10 }}
+            position="relative"
+            zIndex={99}
+            bgcolor="#fff"
+            borderRadius="15px"
+            spacing={10}
+            boxShadow="0 0 12px rgba(0,0,0,0.1)"
+          >
+            <SearchInput />
+            <SearchCard />
+          </Stack>
+        </Container>
+      </Box>
+    </Box>
   );
 }
-
-export default HomePage;
